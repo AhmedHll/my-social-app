a668819ad80b2c00025c3db2316c182ecd892088
AhmedHall <omdadesginer@gmail.com>
2023-01-12 23:24:25 +0200

    upload social app

---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,10 @@
+import { Box } from '@mui/material'
+import { styled } from '@mui/system'
+
+const FlexBetween = styled(Box)({
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    display: 'flex'
+})
+
+export default FlexBetween;
